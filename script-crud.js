4972a09a9e52fe860e01e5580b76bb28b00a305b
Joao Mateus Pinho da Silva <joaomateus.sup.pack@alterdata.com.br>
2026-08-19 11:15:33 -0300

    adiciona mais funções

--- script-crud.js
+++ script-crud.js
@@ -5,22 +5,40 @@ const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textArea = document.querySelector(".app__form-textarea");
 const ulTarefas = document.querySelector(".app__section-task-list");
 
-const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []; //programação defensiva para caso não tenha nenhuma tarefa, mostre a lista vazia
+
+function atualizarTarefas() {
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
 
 function criarElementoTarefa(tarefa) {
   const li = document.createElement("li");
   li.classList.add("app__section-task-list-item");
 
   const svg = document.createElement("svg");
-  svg.innerHTML = `<svg>
+  svg.innerHTML = `
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-        </svg>
-    </svg>`;
+        </svg>`;
   const paragrafo = document.createElement("p");
   paragrafo.textContent = tarefa.descricao;
+  paragrafo.classList.add("app__section-task-list-item-description");
+
   const botao = document.createElement("button");
+  botao.classList.add("app_button-edit");
+
+  botao.onclick = () => {
+    debugger;
+    const novaDescricao = prompt("Qual é o novo nome da tarefa?");
+    console.log("nova descrição da tarefa :", novaDescricao);
+    if (novaDescricao) {
+      paragrafo.textContent = novaDescricao;
+      tarefa.descricao = novaDescricao;
+      atualizarTarefas();
+    }
+  };
+
   const imagemBotao = document.createElement("img");
 
   imagemBotao.setAttribute("src", "/imagens/edit.png");
@@ -41,10 +59,14 @@ btnAdicionarTarefa.addEventListener("click", () => {
 formAdicionarTarefa.addEventListener("submit", (evento) => {
   evento.preventDefault();
   const tarefa = {
-    descicao: textArea.value,
+    descricao: textArea.value,
   };
   tarefas.push(tarefa);
-  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  const elementoTarefa = criarElementoTarefa(tarefa);
+  ulTarefas.append(elementoTarefa);
+  atualizarTarefas();
+  textArea.value = "";
+  formAdicionarTarefa.classList.add("hidden");
 });
 
 tarefas.forEach((tarefa) => {
